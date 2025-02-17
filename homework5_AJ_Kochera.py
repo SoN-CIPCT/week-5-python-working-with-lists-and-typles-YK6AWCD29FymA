7bb77433e381fc502928b84f2ad8fd4367299683
YK6AWCD29FymA <akochera@uw.edu>
2025-02-17 00:57:18 +0000

    Submitting properly

---
+++ homework5_AJ_Kochera.py
@@ -0,0 +1,21 @@
+potatoes = ['red', 'purple', 'russet', 'baking', 'sweet', 'idaho']
+print(potatoes)
+ns1 = potatoes[0]
+ns2 = potatoes[1]
+print("the first two items in the list are: " + ns1 + ", " + ns2)
+ns3 = potatoes[2]
+ns4 = potatoes[3]
+print("the middle two items in the list are: " + ns3 + ", " + ns4)
+print("The first and last items in the list are: " + potatoes[0] + ", " + potatoes[5])
+
+foods = ('pasta', 'onion', 'lamb', 'dandelion', 'cook')
+print("Our menu items are as follows:")
+for foods in foods:
+    print(foods)
+
+# foods(4) = ("Cookie")
+
+foods = ('pasta', 'onion', 'lamb', 'boiled egg', 'chocolate')
+print("\nHere is our updated menu: ")
+for foods in foods:
+    print(foods)
